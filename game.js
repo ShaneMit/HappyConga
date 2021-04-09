df9cf409c4f3f79c3f91e6b5afc8815b1c1d03e4
ShaneMit <shanemit@cox.net>
2021-04-08 22:19:29 -0700

    created snake.js

--- game.js
+++ game.js
@@ -1,11 +1,27 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+
 let lastRenderTime = 0
 
+
 function main(currentTime) {
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
   window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+  console.log('Render')
   lastRenderTime = currentTime
-  console.log(currentTime)
-  console.log(secondsSinceLastRender)
+
+  update()
+  draw()
+  
 }
 
 window.requestAnimationFrame(main)
+
+function update() {
+  updateSnake()
+}
+
+function draw() {
+  drawSnake()
+}
